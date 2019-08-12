c71f1858ddcde58e1085fa591e661b14fb44a269
Marto_0 <martvanenck1@gmail.com>
2019-08-12 13:22:12 +0200

    statistics

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let length = 5
+let numberLength = 5
 let database = {}
 let username = ''
+let backgroundColor = 'rgb(230, 230, 230)'
 let db
 let score = {
   correct: 0,
@@ -13,12 +14,113 @@ $(() => {
   
   db = firebase.firestore()
 
+  $('#username_input').on('change', () => {
+    $('#statistics_button').html(`See Statistics of ${$('#username_input').val()}`)
+  })
+
+  $('#statistics_button').on('click', () => {
+    db.collection("brainTrain-users").doc($('#username_input').val()).collection('scores').get().then((coll) => {
+      let userScores = {}
+      coll.forEach((doc) => { userScores[doc.id] = doc.data() })
+      showStatistics(userScores)
+    })
+    $('#body').html(' ')
+  })
+
 })
 
+function showStatistics(userScores) {
+  console.log(userScores)
+
+    // create average
+  $('#body').append(`
+<div id="average">
+  <div class="header">AVERAGE PER LENGTH</div>
+  <table>
+    <tr>
+      <th>Length</th>
+      <th>Score</th>
+    </tr>
+  </table>
+</div>
+`)
+
+  // get scores per length
+  let scoresPerLength = {}
+  for(let dateMS in userScores) {
+    let score = userScores[dateMS]
+    if(scoresPerLength[score.length] == undefined) scoresPerLength[score.length] = []
+    scoresPerLength[score.length].push(score.percentage)
+  }
+
+  // calculate average per length
+  let averagePerLength = {}
+  for(let length in scoresPerLength) {
+    let scoreCount = scoresPerLength[length].length
+    let total = 0
+    for(let score of scoresPerLength[length]) total += score
+
+    let average = total / scoreCount
+    averagePerLength[length] = average
+  }
+  console.log(averagePerLength)
+
+  // add averages to table
+  for(let length in averagePerLength) {
+    let average = averagePerLength[length]
+
+    $('#average table').append(`
+<tr class="cell" id="cell-${length}">
+  <td>${length}</td>
+  <td class="score">
+    <div class="scoreBar">
+      <div class="scoreNum">${average}%</div>
+      <div class="score" style="width: ${average}%;"></div>
+    </div>
+  </td>
+</tr>
+    `)
+
+  }
+
+  // create history
+  $('#body').append(`
+<div id="history">
+  <div class="header">SCORES HISTORY</div>
+  <div class="scores"></div>
+</div>
+`)
+
+  // add all scores to history
+  for(let dateMS in userScores) {
+    let score = userScores[dateMS]
+
+    let date = new Date()
+    date.setTime(dateMS)
+
+    let dateSegments = date.toString().split(' ')
+    let dateString = dateSegments[4]+' '+dateSegments[1]+' '+dateSegments[2]+' '+dateSegments[3]
+
+    $('#history .scores').append(`
+<div id="score-${dateMS}" class="score">
+  <div class="date">DATE: ${dateString}</div>
+  <div class="scoreBar">
+    <div class="scoreNum">${score.percentage}%</div>
+    <div class="score"></div>
+  </div>
+  <div class="date">LENGTH: ${score.length}</div>
+  <hr>
+</div>
+    `)
+
+    $(`#score-${dateMS} .scoreBar .score`).css('width', `${score.percentage}%`)
+
+  }
+
+}
 
 
 function start() {
-  //getDatabase()
 
   // reset score
   score.correct = 0
@@ -26,7 +128,7 @@ function start() {
   score.tries = 0
 
   // other
-  $('body').css('background-color', 'white')
+  $('body').css('background-color', backgroundColor)
   $('#username_input').val('')
   $('#length_input').val(5)
 
@@ -39,7 +141,7 @@ function start() {
     $('#menu').hide()
     $('#score').show()
     $('#text').show()
-    length = $('#length_input').val()
+    numberLength = $('#length_input').val()
     username = $('#username_input').val()
 
     next()
@@ -54,13 +156,13 @@ function calculatePercentage() {
 }
 
 async function next() {
-  let random = randomString(length)
+  let random = randomString(numberLength)
 
-  $('body').css('background-color', 'white')
+  $('body').css('background-color', backgroundColor)
   $('#text').html(random)
   $('#input').hide()
 
-  let time = length*200
+  let time = numberLength*200
   if(time < 1000) time = 1000
   await sleep(time)
 
@@ -97,14 +199,14 @@ async function next() {
 
         database[username][time] = {
           'percentage': percentage,
-          'length': length
+          'length': numberLength
         }
 
         // Add a new document in collection "cities"
         if(username != '') {
           db.collection("brainTrain-users").doc(username).collection('scores').doc(time.toString()).set({
             'percentage': percentage,
-            'length': length
+            'length': numberLength
           })
           .then(() => {
             console.log("Document successfully written!")
